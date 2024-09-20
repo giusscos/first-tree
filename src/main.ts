b1abb6d6f6141ed524ec56c7a8bff69e444b983c
giusscos <giu.cosenza08@gmail.com>
2024-09-20 18:28:45 +0200

    Add draw line function and minor fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import './style.css'
 
+import WebGL from 'three/addons/capabilities/WebGL.js';
 import * as THREE from 'three';
 
 const innerWidth = window.innerWidth;
@@ -8,6 +9,10 @@ const innerHeight = window.innerHeight;
 const scene = new THREE.Scene();
 const aspectRatio = innerWidth / innerHeight
 const camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000);
+// const camera = new THREE.PerspectiveCamera(45, aspectRatio, 0.1, 500);
+
+camera.position.set( 0, 0, 100 );
+camera.lookAt( 0, 0, 0 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(innerWidth, innerHeight);
@@ -18,9 +23,21 @@ const material = new THREE.MeshNormalMaterial();
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
-camera.position.y = 1
+//create a blue LineBasicMaterial
+// const material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
 
+// const points = [];
+// points.push( new THREE.Vector3( - 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, 10, 0 ) );
+// points.push( new THREE.Vector3( 10, 0, 0 ) );
+// points.push( new THREE.Vector3( 0, -10, 0 ) );
+// points.push( new THREE.Vector3( -10, 0, 0 ) );
+
+// const geometry = new THREE.BufferGeometry().setFromPoints( points );
+// const line = new THREE.Line( geometry, material );
+// scene.add(line);
+
+camera.position.z = 5;
 
 function animate() {
   cube.rotation.x += 0.01;
@@ -28,10 +45,8 @@ function animate() {
 
   renderer.render(scene, camera);
 }
-import WebGL from 'three/addons/capabilities/WebGL.js';
 
 if (WebGL.isWebGL2Available() ) {
-
 	// Initiate function or other initializations here
 	// animate();
 
